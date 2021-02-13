28790fff41eea45496c48108ce638f6d6f88b290
aii23 <a.ivlev.om@gmail.com>
2021-02-13 18:37:02 +0300

    blockchain.js complete

--- src/blockchain.js
+++ src/blockchain.js
@@ -34,7 +34,7 @@ class Blockchain {
      * Passing as a data `{data: 'Genesis Block'}`
      */
     async initializeChain() {
-        if( this.height === -1){
+        if(this.height === -1){
             let block = new BlockClass.Block({data: 'Genesis Block'});
             await this._addBlock(block);
         }
@@ -64,7 +64,13 @@ class Blockchain {
     _addBlock(block) {
         let self = this;
         return new Promise(async (resolve, reject) => {
-           
+            block.previousBlockHash = self.height >= 0 ? self.chain[self.height].hash : null; 
+            block.time = new Date().getTime().toString().slice(0, -3);
+            self.height += 1;
+            block.height = self.height;
+            block.hash = SHA256(JSON.stringify(block)).toString();
+            self.chain.push(block);
+            resolve(block);
         });
     }
 
@@ -78,7 +84,7 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-            
+            resolve(`${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry`);
         });
     }
 
@@ -102,7 +108,31 @@ class Blockchain {
     submitStar(address, message, signature, star) {
         let self = this;
         return new Promise(async (resolve, reject) => {
-            
+            try 
+            {
+                let messageTime = parseInt(message.split(':')[1]);
+                let currentTime = parseInt(new Date().getTime().toString().slice(0, -3));
+
+                if (currentTime - messageTime > 5 * 60) {
+                    reject("Time is out");
+                }
+
+                if (!bitcoinMessage.verify(message, address, signature)) {
+                    reject("Wrong signature");
+                }
+
+                let data = {
+                    "star": star, 
+                    "sender": address
+                };
+
+                let block = new BlockClass.Block(data);
+                self._addBlock(block)
+                .then(resolve(block));
+            } catch(e) {
+                console.log(e);
+                reject(`Cannot submit star. Reason: ${e.message}`);
+            }
         });
     }
 
@@ -115,7 +145,12 @@ class Blockchain {
     getBlockByHash(hash) {
         let self = this;
         return new Promise((resolve, reject) => {
-           
+            let result = self.chain.filter((elem) => elem.hash == hash)[0];
+            if (result) {
+                resolve(result);
+            } else {
+                reject(`No block with hash ${hash}`);
+            }
         });
     }
 
@@ -131,7 +166,7 @@ class Blockchain {
             if(block){
                 resolve(block);
             } else {
-                resolve(null);
+                resolve(`No block with height ${height}`);
             }
         });
     }
@@ -145,8 +180,14 @@ class Blockchain {
     getStarsByWalletAddress (address) {
         let self = this;
         let stars = [];
-        return new Promise((resolve, reject) => {
-            
+        return new Promise(async (resolve, reject) => {
+            let decodedPromises = self.chain.slice(1).map(block => block.getBData().catch(e => console.log(e)));
+
+            let decoded = await Promise.all(decodedPromises);
+
+            resolve(decoded
+                .filter(body => body.sender == address)
+                .map(body => body.star));
         });
     }
 
@@ -160,7 +201,23 @@ class Blockchain {
         let self = this;
         let errorLog = [];
         return new Promise(async (resolve, reject) => {
-            
+            /// Create array of Promises to validate block
+            let validationArray = self.chain.map((block, i) => 
+                block.validate()
+                    .then(blockValidationResult => {
+                        if (!blockValidationResult) {
+                            errorLog.push(`Block ${block.hash} at position ${i} has wrong hash`);
+                        }
+
+                        if (i > 0  && self.chain[i-1].hash != block.previousBlockHash) {
+                            errorLog.push(`Block ${block.hash} at position ${i} has wrong previousBlockHash`);
+                        }
+                    })
+                    .catch(error => 
+                        errorLog.push(`System Error ${error.name} : ${error.message}`))
+            );
+
+            Promise.all(validationArray).then(() => resolve(errLog));
         });
     }
 
