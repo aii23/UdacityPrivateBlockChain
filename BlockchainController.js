41d08dc24f4b9a2d1346ef006938067fd07655f6
aii23 <a.ivlev.om@gmail.com>
2021-02-14 11:22:06 +0300

    Validate Chain endpoint added

--- BlockchainController.js
+++ BlockchainController.js
@@ -17,6 +17,7 @@ class BlockchainController {
         this.submitStar();
         this.getBlockByHash();
         this.getStarsByOwner();
+        this.validateChain();
     }
 
     // Enpoint to Get a Block by Height (GET Endpoint)
@@ -118,6 +119,17 @@ class BlockchainController {
         });
     }
 
+    // This endpoint allows you to get list of all chain validation error 
+    validateChain() {
+        this.app.get("/validateChain", async (req, res) => {
+            try {
+                let validationResult = await this.blockchain.validateChain();
+                return res.status(200).json(validationResult);
+            } catch (error) {
+                return res.status(500).send("Error while validation chain");
+            }
+        })
+    }
 }
 
 module.exports = (app, blockchainObj) => { return new BlockchainController(app, blockchainObj);}
